10e179c98b094f594b235fb623c0a70ea3050f27
sisihuang <276023092@qq.com>
2021-01-27 14:38:25 +0800

    Idea整合github测试1

---
+++ basic-code/day04-code/src/cn/itcast/day04/demo01/HalloWorld.java
@@ -0,0 +1,12 @@
+package cn.itcast.day04.demo01;
+
+
+public class HalloWorld {
+
+    public static void main(String[] args) {
+        char a = '\u0061' ;
+        System.out.println(a);
+
+    }
+
+}
